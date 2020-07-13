566e9d4523ad86c1a7f30fb12f2041afb433a57a
mmarques <matheus.luiz@fcamara.com.br>
2020-07-13 20:40:15 -0300

    [CriarUsuario] Estilo formulário cadastro usuário

--- front/src/app/components/usuario/criar-usuario/criar-usuario.component.ts
+++ front/src/app/components/usuario/criar-usuario/criar-usuario.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarUsuarioComponent implements OnInit {
 
+  propLegal = "qualquer";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fazerAlgo(): void {
+    console.log('fazer algo')
   }
 
 }
