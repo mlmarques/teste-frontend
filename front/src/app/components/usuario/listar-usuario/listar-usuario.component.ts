0e09a5b3f46936c0f7661fc625bde21ca7d53990
mmarques <matheus.luiz@fcamara.com.br>
2020-07-14 20:30:07 -0300

    [ExcluirUsuarios] Metódos e logica de excluir usuário

--- front/src/app/components/usuario/listar-usuario/listar-usuario.component.ts
+++ front/src/app/components/usuario/listar-usuario/listar-usuario.component.ts
@@ -15,9 +15,27 @@ export class ListarUsuarioComponent implements OnInit {
   constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
-    this.usuarioService.buscarUsuarios().subscribe(usuarios => {
-      this.usuarios = usuarios;
-    })
+    // this.usuarioService.consumirDadosIniciais().subscribe(usuarios => {
+    //   this.usuarios = usuarios;
+    // })
+
+    // -----------------------------------------------------------------
+    this.usuarioService.setDadosIniciar();
+    this.usuarios = this.usuarioService.getListaUsuarios();
+    console.log(this.usuarioService.getUsuarioPorCpf(this.usuarios[0].cpf));
+  }
+
+  editarUsuario(cpf: number): void{
+    console.log(this.usuarioService.getUsuarioPorCpf(cpf));
+  }
+
+  excluirUsuario(cpf: number): void{
+    this.usuarioService.deleteUsuario(cpf);
+    this.atualizarDados();
+  }
+
+  atualizarDados(): void{
+    this.usuarios = this.usuarioService.getListaUsuarios();
   }
 
 }
--- front/src/app/components/usuario/usuario.service.ts
+++ front/src/app/components/usuario/usuario.service.ts
@@ -19,8 +19,42 @@ export class UsuarioService {
     console.log(usuario);
   }
 
-  buscarUsuarios(): Observable<Usuario[]>{
+  buscarUsuarios(): any {
+  }
+
+  consumirDadosIniciais(): Observable<Usuario[]>{
     return this.http.get<Usuario[]>(this.url);
   } 
+
+  setDadosIniciar(): void{
+    this.consumirDadosIniciais().subscribe(usuarios => {
+      this.criarStorage(usuarios);
+    })
+  }
+
+  getListaUsuarios(): any{
+    return JSON.parse(localStorage.getItem('usuarios'));
+  }
+
+  getUsuarioPorCpf(cpf: number): any{
+    return this.getListaUsuarios().find(el => el.cpf === cpf);
+  }
+
+  createUsuario(usuario: Usuario): void{
+    this.getListaUsuarios().push(usuario);
+  }
   
+  deleteUsuario(cpf: number): void{
+    let novaLista = this.getListaUsuarios().splice(1, this.getListaUsuarios().map(el => {return el.cpf}).indexOf(cpf));
+    this.limparStorage();
+    this.criarStorage(novaLista);
+  }
+
+  limparStorage(): void{
+    localStorage.removeItem('usuarios');
+  }
+
+  criarStorage(usuarios: Usuario[]): void{
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+  }
 }
